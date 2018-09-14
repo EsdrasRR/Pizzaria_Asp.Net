240a2303ab70b909f8f5fc0a688a200d96ef7a38
Esdras Rafael <esdrasrocha1122@gmail.com>
2018-09-14 09:03:41 -0300

    Criando classe de Contexto

---
+++ MakePizza/MakePizza/Models/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MakePizza.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(): base("DbMakePizza") { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Ingrediente> Ingedientes { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+    }
+}
