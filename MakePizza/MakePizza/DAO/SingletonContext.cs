b19bf14592d76513c6c8af83847410bb18074789
Esdras Rafael <esdrasrocha1122@gmail.com>
2018-09-14 09:09:04 -0300

    Criando pasta DAO

---
+++ MakePizza/MakePizza/DAO/SingletonContext.cs
@@ -0,0 +1,24 @@
+﻿using MakePizza.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MakePizza.DAO
+{
+    public class SingletonContext
+    {
+        private static Contexto contexto;
+
+        private SingletonContext() { }
+
+        public static Contexto GetInstance()
+        {
+            if(contexto == null)
+            {
+                contexto = new Contexto();
+            }
+            return contexto;
+        }
+    }
+}
